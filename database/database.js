380b8e1ab00a8fd15089543b939ad4deadcc658c
Marcelo Alves <alvesma@gmail.com>
2022-03-01 20:04:03 -0300

    Sist de Blog

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const sequelize = require("sequelize")
 
 const connection = new sequelize('guiapress','root','wcawca#023',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 
 })
 module.exports = connection
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const articlesController = require("./articles/ArticlesController")
 
 const Article = require("./articles/Article")
 const Category = require("./categories/Category")
+const router = require("./categories/CategoriesController")
 
 app.set('view engine','ejs')
 
@@ -30,9 +31,60 @@ app.use("/", articlesController)
 })
 
 app.get("/",(req,res)=>{
-
-    res.render('index')
+    Article.findAll({
+        order: [['id', 'DESC']
+    ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories})
+        })
+        
+    })
+   
 })
+
+app.get("/:slug", (req,res) =>{
+var slug = req.params.slug
+Article.findOne({
+    where: {
+        slug: slug
+    }
+}).then(article => {
+    if(article != undefined){
+        Category.findAll().then(categories => {
+            res.render("article", {article: article, categories: categories})
+        })
+    }else {
+        res.redirect("/")
+    }
+}).catch( err => {
+    res.redirect("/")
+    })
+
+})
+app.get("/category/:slug", (req,res) =>{
+    var slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            })
+
+        }else {
+            res.redirect("/")
+        }
+    }).catch(err =>{
+        res.redirect("/")
+    })
+
+})
+
 app.listen(8080,()=>{
     console.log("servidor ok")
 })
